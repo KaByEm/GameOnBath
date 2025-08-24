9d4a1270bad53b9523a2b58e329babe29587aff4
Bilal <69146100+KaByEm@users.noreply.github.com>
2025-08-24 20:40:38 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("bookingForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  document.getElementById("formMessage").textContent = "✅ Booking successful! We'll email you details shortly.";
-  document.getElementById("formMessage").style.color = "green";
-  this.reset();
-});
